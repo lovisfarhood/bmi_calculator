36a8bd6a2c370cd73bbdb71beef6c8907587b113
lovisfarhood <lovis.hildebrand@gmail.com>
2025-01-28 12:12:37 +0100

    check for inputerror and use of round()

--- code/body_maß_index.py
+++ code/body_maß_index.py
@@ -1,7 +1,20 @@
 print ("Willkommen zu BMI-Farhood. ")
-koerpergroesse = float(input("Um ihren Body Maß Index zu berechnen, geben sie bitte zunächst ihre Körpergröße in Cm an: "))
-koerpergewicht = float(input("Um fortzufahren, geben sie bitte nun ihr Körpergewicht in Kg an: "))
+while True:
+    koerpergroesse = (input("Um ihren Body Maß Index zu berechnen, geben sie bitte zunächst ihre Körpergröße in Cm an: "))
+    if koerpergroesse.isdigit():
+        koerpergroesse = int(koerpergroesse)
+        break
+    else:
+        print("Ungültige Eingabe. Bitte versuchen Sie es erneut:")
+while True:
+    koerpergewicht = (input("Um fortzufahren, geben sie bitte nun ihr Körpergewicht in Kg an: "))
+    if koerpergewicht.isdigit():
+        koerpergewicht = int(koerpergewicht)
+        break
+    else:
+        print:("UngUngültige Eingabe. Bitte versuchen Sie es erneut:")
 
 bodymaßindex = koerpergewicht / (koerpergroesse / 100) ** 2
+bmi_gerundet = round(bodymaßindex,1)
 
-print ("Dein Bodymaßindex lautet", bodymaßindex,"\nViel spaß damit.")
+print ("Dein Bodymaßindex lautet", bmi_gerundet,"\nViel spaß damit.")
