e9814147b720f8d23c45d70ad4a3b36f8d182ef5
lovisfarhood <lovis.hildebrand@gmail.com>
2025-01-29 15:52:21 +0100

    dezimalzahlen mit r"^\d+(\.\d+)?$" erlauben

--- code/body_maß_index.py
+++ code/body_maß_index.py
@@ -1,16 +1,20 @@
+import re
+
+muster = r"^\d+(\.\d+)?$"
+
 print ("Willkommen zu BMI-Farhood. ")
 while True:
     koerpergroesse = (input("Um ihren Body Maß Index zu berechnen, geben sie bitte zunächst ihre Körpergröße in Cm an: "))
-    if koerpergroesse.isdigit():
-        koerpergroesse = int(koerpergroesse)
+    if re.match(muster, koerpergroesse):
+        koerpergroesse = float(koerpergroesse)
         break
     else:
         print("Ungültige Eingabe. Bitte versuchen Sie es erneut:")
 
 while True:
     koerpergewicht = (input("Um fortzufahren, geben sie bitte nun ihr Körpergewicht in Kg an: "))
-    if koerpergewicht.isdigit():
-        koerpergewicht = int(koerpergewicht)
+    if re.match(muster, koerpergewicht):
+        koerpergewicht = float(koerpergewicht)
         break
     else:
         print("Ungültige Eingabe. Bitte versuchen Sie es erneut:")
